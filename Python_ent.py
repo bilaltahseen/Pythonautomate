5cf47ba98a303faf4f9907750ff40a141df2dfe5
Bilal <bilaltehseen@gmail.com>
2019-08-20 01:32:48 +0500

    Version 2.0

--- Python_ent.py
+++ Python_ent.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pygubu
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 s = open('Details.txt','w')
 class Application:
     def __init__(self, master):
@@ -20,31 +21,46 @@ class Application:
         builder.connect_callbacks(self)
     def print_event(self):
         text_box=self.builder.get_object('Entry_1')
-        num_pages=self.builder.get_object("Entry_2")
-        label_print = self.builder.get_object('Label_1')
+        label_print=self.builder.get_object("Label_1")
         ent_text = text_box.get()
-        num_pages_val = num_pages.get()
         chrome_opts = Options()
         chrome_opts.add_argument('--headless')
         driver = webdriver.Chrome("./chromedriver",options=chrome_opts)
         driver.get('https://www.google.com')
         driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input').send_keys(ent_text)
         driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div/div[3]/center/input[1]').click()
-        s = open("Details.txt","w")
+        s = open("Details.doc","w",encoding="utf-8")
         
-        for i in range(int(num_pages_val)):
-           search_list=driver.find_element_by_id('search')
-           op_val = search_list.text
-           list_elem=[]
-           list_elem.append(op_val)
-           driver.find_element_by_xpath('//*[@id="pnnext"]').click()
-           
-        for x in list_elem:
-            s.writelines(str(x))   
-        label_print.configure(text='Data saved')
-        s.close
+        
+        while True:
+           try:
+               button_1=self.builder.get_object('Button_1')
+               button_1.config(state="disabled")
+               search_list=driver.find_element_by_id('search')
+               s.writelines(str(search_list.text))
+               driver.find_element_by_xpath('//*[@id="pnnext"]').click()
 
-        
+               
+               
+
+           except NoSuchElementException:
+               button_1=self.builder.get_object('Button_1')
+               button_1.config(state="normal")
+               label_print.config(text="Done Saving.")
+               s.close
+               break
+               
+
+      
+               
+       
+          
+
+
+
+           
+           
+
         
 
 
